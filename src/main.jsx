2dd83001aa4e044c615576c548f5faf015c9d76c
dhruv08varaiya <dhruv.varaiya@gmail.com>
2026-03-12 00:20:05 +0530

    feat: configure app for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 // Styles
@@ -11,8 +11,8 @@ import './styles/bootstrap-overrides.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
